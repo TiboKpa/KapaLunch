4ac191db60c3af0765ebb7860b23cc01029d42b0
TkPa <48863741+TiboKpa@users.noreply.github.com>
2025-12-20 12:51:42 +0100

    Ajouter bouton croix pour effacer la recherche

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -221,6 +221,20 @@ function Header({ user, onLogin, onLogout, onToggleAddForm, showUserPanel, setSh
               className="search-input-header"
             />
             
+            {/* Bouton croix pour effacer */}
+            {searchTerm && (
+              <button 
+                className="btn-clear-search"
+                onClick={() => setSearchTerm('')}
+                title="Effacer la recherche"
+                type="button"
+              >
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                </svg>
+              </button>
+            )}
+            
             {/* Bouton Filtres */}
             <button 
               className="btn-filter-header"
